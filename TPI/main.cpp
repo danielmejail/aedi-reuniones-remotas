2757d29c51b73a2d9088e0bd2a55f9dc06740154
Daniel Mejail <dailandylan@gmail.com>
2020-07-15 13:41:28 -0300

    modificar main.cpp

--- TPI/main.cpp
+++ TPI/main.cpp
@@ -3,37 +3,38 @@
 #include <iostream>
 #include <string>
 
-//#include "gtest/gtest.h"
+#include "gtest/gtest.h"
 
-#include "TPI-tests/test_suites.h"
 #include "definiciones.h"
 #include "ejercicios.h"
 #include "auxiliares.h"
-#include "TPI-tests/Test.h"
+//#include "TPI-tests/test_suites.h"
+//#include "TPI-tests/Test.h"
 
 using namespace std;
 
-/*
+
 int main(int argc, char **argv) {
     std::cout << "Implementando TPI!!" << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-*/
 
 
+/*
 int main(void){
-	//iniciarTestsEjercicio01();
+	iniciarTestsEjercicio01();
 	iniciarTestsEjercicio02();
-	//iniciarTestsEjercicio03();
-	//iniciarTestsEjercicio04();
+	iniciarTestsEjercicio03();
+	iniciarTestsEjercicio04();
 	iniciarTestsEjercicio05();
-	//iniciarTestsEjercicio06();
-	//iniciarTestsEjercicio07();
+	iniciarTestsEjercicio06();
+	iniciarTestsEjercicio07();
 	iniciarTestsEjercicio08();
-	//iniciarTestsEjercicio09();
-	//iniciarTestsEjercicio10();
+	iniciarTestsEjercicio09();
+	iniciarTestsEjercicio10();
 	iniciarTestsEjercicio11();
 	return 0;
 }
+*/
 
